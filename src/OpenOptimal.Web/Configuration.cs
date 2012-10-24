4dc247f452dfdb8aa421630c23976255b90f0f43
Ben O'Hara <bpo@robotparade.co.uk>
2012-10-24 12:11:38 +0100

    Enabled webforms codec.

--- src/OpenOptimal.Web/Configuration.cs
+++ src/OpenOptimal.Web/Configuration.cs
@@ -7,9 +7,12 @@ using System.Text;
 using System.Web;
 using System.Xml;
 using System.Xml.Linq;
+using OpenOptimal.Web.Handlers;
+using OpenOptimal.Web.Resources;
 using OpenRasta.Configuration;
 using OpenRasta.Web;
 using OpenRasta.Codecs;
+using OpenRasta.Codecs.WebForms;
 
 namespace OpenOptimal.Web
 {
@@ -17,8 +20,11 @@ namespace OpenOptimal.Web
     {
         public void Configure()
         {
-            ResourceSpace.Uses.LinkingXmlDataContract();
-            ResourceSpace.Uses.ConventionsFrom(this);
+        	ResourceSpace.Has.Resource<Home>()
+        		.Uri("/")
+        		.And.Uri("/home")
+        		.Handler<HomeHandler>()
+        		.RenderedByAspx("~/Views/HomeView.aspx");
         }
     }
 }
---
+++ src/OpenOptimal.Web/Views/HomeView.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page
+Language           = "C#"
+Inherits           = "OpenRasta.Codecs.WebForms.ResourceView<Home>"
+Title			   = "Op Optimal"
+%>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  	<title>Op-Optimal</title>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="IE=8">
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+  	<h1>Op Optimal</h1>
+  </body>
+</html>
