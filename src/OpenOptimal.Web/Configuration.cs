1a26402dc33c0a9381535072252c08589e9a2037
Ben O'Hara <bpo@robotparade.co.uk>
2012-10-24 11:02:55 +0100

    created OpenRasta site from template.

---
+++ src/OpenOptimal.Web/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Web;
+using System.Xml;
+using System.Xml.Linq;
+using OpenRasta.Configuration;
+using OpenRasta.Web;
+using OpenRasta.Codecs;
+
+namespace OpenOptimal.Web
+{
+    public class Configuration : IConfigurationSource
+    {
+        public void Configure()
+        {
+            ResourceSpace.Uses.LinkingXmlDataContract();
+            ResourceSpace.Uses.ConventionsFrom(this);
+        }
+    }
+}
